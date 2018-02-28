0a0dbf23393899339a4bf21ccc0b0863dec6429c
rikutech <rikutech@gmail.com>
2018-03-01 02:57:43 +0900

    prepare to implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::io::{BufWritetr, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("generating empty zip...");
+    //TODO: implement zip generation
+    println!("empty.zip generated!!!!!!!");
+}
+
+struct EndOfCentralDirectoryRecord {
+    signature: u32,
+    disk: u16,
+    central_dir_disk: u16,
+    entry_number: u16,
+    total_entry_number: u16,
+    central_dir_size: u32,
+    central_dir_offset: u32,
+    comment_length: u16
 }
